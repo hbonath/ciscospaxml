70616180e650185fbfe9d6b26fa60090d4da5378
Henry Bonath <henry@thebonaths.com>
2014-07-20 17:33:40 -0400

    Added FreePBX Bootstrap

--- directory/spadir.php
+++ directory/spadir.php
@@ -1,24 +1,28 @@
 <?php
-$con=mysqli_connect("localhost","username","password","database");
-
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// load FreeBPX bootstrap environment, requires FreePBX 2.9 or higher
+if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
+include_once('/etc/asterisk/freepbx.conf');
 }
-
-$result = mysqli_query($con,"SELECT * FROM users");
-
+ 
+// set FreePBX globals
+global $db;  // FreePBX asterisk database connector
+global $amp_conf;  // array with Asterisk configuration
+global $astman;  // AMI
+ 
+$sql = "SELECT * FROM users";
+$results = $db->getAll($sql, DB_FETCHMODE_ASSOC);  // 2D array of all FreePBX users
+ 
 header ("content-type: text/xml");
     echo "<CiscoIPPhoneDirectory>\n";
     echo "<Title>PBX Directory</Title>\n";
     echo "<Prompt>Select a User</Prompt>\n";
-	while($row = mysqli_fetch_array($result)) {
-	  echo "<DirectoryEntry>\n";
-	  echo "<Name>" . $row['name'] . "</Name>\n";
-	  echo "<Telephone>" . $row['extension'] . "</Telephone>\n";
-	  echo "</DirectoryEntry>\n";
-	}
+foreach  ($results as $row) {
+    echo "<DirectoryEntry>\n";
+    echo "<Name>" . $row['name'] . "</Name>\n";
+    echo "<Telephone>" . $row['extension'] . "</Telephone>\n";
+    echo "</DirectoryEntry>\n";
+}
     echo "</CiscoIPPhoneDirectory>\n";
+ 
 
-
-mysqli_close($con);
 ?>
