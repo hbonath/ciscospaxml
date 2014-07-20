7e660df7d6bd4ec9e85aec20dd902c7dc85b254b
Henry Bonath <henry@thebonaths.com>
2014-07-20 18:32:42 -0400

    Added check for <32 users Cisco only supports 32 results with <CiscoIPPhoneDirectory>

--- directory/spadir.php
+++ directory/spadir.php
@@ -11,18 +11,26 @@ global $astman;  // AMI
  
 $sql = "SELECT * FROM users";
 $results = $db->getAll($sql, DB_FETCHMODE_ASSOC);  // 2D array of all FreePBX users
- 
-header ("content-type: text/xml");
-    echo "<CiscoIPPhoneDirectory>\n";
-    echo "<Title>PBX Directory</Title>\n";
-    echo "<Prompt>Select a User</Prompt>\n";
-foreach  ($results as $row) {
-    echo "<DirectoryEntry>\n";
-    echo "<Name>" . $row['name'] . "</Name>\n";
-    echo "<Telephone>" . $row['extension'] . "</Telephone>\n";
-    echo "</DirectoryEntry>\n";
-}
-    echo "</CiscoIPPhoneDirectory>\n";
- 
+$numrows = count($results);
 
+if ($numrows < 33 ) {
+	header ("content-type: text/xml");
+	    echo "<CiscoIPPhoneDirectory>\n";
+	    echo "<Title>PBX Directory</Title>\n";
+	    echo "<Prompt>Select a User</Prompt>\n";
+	foreach  ($results as $row) {
+	    echo "<DirectoryEntry>\n";
+	    echo "<Name>" . $row['name'] . "</Name>\n";
+	    echo "<Telephone>" . $row['extension'] . "</Telephone>\n";
+	    echo "</DirectoryEntry>\n";
+	}
+	    echo "</CiscoIPPhoneDirectory>\n";
+} else {
+        header ("content-type: text/xml");
+	    echo "<CiscoIPPhoneText>\n";
+	    echo "<Title>Result Too Large</Title>\n";
+	    echo "<Prompt>This application is limited to 32 entries</Prompt>\n";
+	    echo "<Text>Your PBX currently has more than 32 extensions, which is beyond the limit of this application.</Text>\n";
+	    echo "</CiscoIPPhoneText>\n";
+}
 ?>
